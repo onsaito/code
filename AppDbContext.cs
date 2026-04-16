5b3ebd8eaff897f30eef117cb285b99565213f4a
onsaito <toru.saito@gmail.com>
2026-04-16 15:53:56 -0700

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FileUploadPOC.Models
+{
+    public class AppDbContext : DbContext
+    {
+        AppDbContext(DbContextOptions<AppDbContext> options) { }
+        public DbSet<UploadedFile> UploadedFiles { get; set; }
+    }
+}
