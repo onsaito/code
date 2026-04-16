7365ba7c1db5a0ca0e0088c9cbd9a2caf64ef5f8
onsaito <toru.saito@gmail.com>
2026-04-16 15:54:44 -0700

    Update AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -4,7 +4,7 @@ namespace FileUploadPOC.Models
 {
     public class AppDbContext : DbContext
     {
-        AppDbContext(DbContextOptions<AppDbContext> options) { }
+        AppDbContext(DbContextOptions<AppDbContext> options) : base(options){ }
         public DbSet<UploadedFile> UploadedFiles { get; set; }
     }
 }
